5f738a170e4c33f3bf7394f4f6a029e694b8dcba
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2025-10-16 22:00:26 +0530

    tools: nvm_tools: adsd3500_fw_update: added changes to update adsd3500 master
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- tools/nvm_tools/adsd3500_fw_update/main.cpp
+++ tools/nvm_tools/adsd3500_fw_update/main.cpp
@@ -9,5 +9,27 @@
 #include "Adsd3500.h"
 
 int main(int argc, char *argv[]) {
-    auto adsd = Adsd3500(argv[1]);
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <file> <master|slave>\n", argv[0]);
+		fprintf(stderr, "       .bin file for master target\n");
+		fprintf(stderr, "       .stream file for slave target\n");
+		return 1;
+	}
+
+	const char *filename = argv[1];
+	const char *target   = argv[2];
+
+	if (strcmp(target, "master") != 0 && strcmp(target, "slave") != 0) {
+		fprintf(stderr, "Error: Second argument must be 'master' or 'slave'.\n");
+		return 1;
+	}
+
+	if (!validate_ext(filename, target)) {
+		fprintf(stderr, "Error: For '%s' target, file must have a '%s' extension.\n",
+				target, strcmp(target, "master") == 0 ? ".bin" : ".stream");
+		return 1;
+	}
+
+	auto adsd = Adsd3500(filename, target);
 }
