fcad71c51d8a018ede08db6df9e9ce97f3a60a67
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2025-10-31 13:26:42 +0530

    tools: debug_apps: ctrl_app: run media-ctl to get subdevice path
    - Execute `media-ctl -d /dev/media0 --print-dot to inspect media topology
    - Parse output to locate the subdevice path corresponding to the target device
    - Store subdevice path in a variable for further use in opening the device
    
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- tools/debug_apps/ctrl_app/ctrl_app.cpp
+++ tools/debug_apps/ctrl_app/ctrl_app.cpp
@@ -21,13 +21,59 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+using namespace std;
+
 #define IOCTL_TRIES 3
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 #define V4L2_CID_ADSD3500_DEV_CHIP_CONFIG (0x009819d1)
 #define CTRL_SIZE 4099
+#define MEDIA_DEVICE 	"/dev/media0"
+
 #define VER_MAJ 1
-#define VER_MIN 0
+#define VER_MIN 1
 #define VER_PATCH 0
+
+bool findDevicePathsAtVideo(const std::string &video,
+		std::string &subdev_path,
+		std::string &device_name) {
+
+	char *buf;
+	int size = 0;
+	size_t pos;
+
+	/* Run media-ctl to get the video processing pipes */
+	char cmd[64];
+	sprintf(cmd, "media-ctl -d %s --print-dot", video.c_str());
+	FILE *fp = popen(cmd, "r");
+	if (!fp) {
+		std::cout << "Error running media-ctl";
+		return false;
+	}
+
+	/* Read the media-ctl output stream */
+	buf = (char *)malloc(128 * 1024);
+	while (!feof(fp)) {
+		fread(&buf[size], 1, 1, fp);
+		size++;
+	}
+	pclose(fp);
+	buf[size] = '\0';
+
+	/* Search command media-ctl for device/subdevice name */
+	string str(buf);
+	free(buf);
+
+
+	if (str.find("adsd3500") != string::npos) {
+		device_name = "adsd3500";
+		pos = str.find("adsd3500");
+		subdev_path = str.substr(pos + strlen("adsd3500") + 9,
+				strlen("/dev/v4l-subdevX"));
+	} else {
+		return false;
+	}
+	return true;
+}
 
 static int xioctl(int fd, int request, void *arg) {
     int r;
@@ -77,8 +123,19 @@ bool v4l2_ctrl_get(int fd, uint32_t id, uint8_t *val) {
 
 int main(int argc, char **argv) {
     uint8_t data[CTRL_SIZE] = {0};
+    int status;
 
-    int fd = open("/dev/v4l-subdev1", O_RDWR | O_NONBLOCK);
+    std::string video = "/dev/media0";
+    std::string deviceName = "adsd3500";
+    std::string subdevPath;
+
+    status = findDevicePathsAtVideo(video, subdevPath, deviceName);
+    if (!status) {
+	    std::cout << "failed to find device paths at video: " << video;
+	    return status;
+    }
+
+    int fd = open(subdevPath.c_str(), O_RDWR | O_NONBLOCK);
     if (fd == -1) {
         std::cout << "Failed to open the camera" << std::endl;
         return -1;
