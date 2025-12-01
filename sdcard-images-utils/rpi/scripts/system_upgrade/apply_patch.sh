28bbe6228262e3f826cd228891bdd7f73493d728
Sivasubramaniyan-Padmanaban_adi <sivasubramaniyan.padmanaban@analog.com>
2025-12-01 16:24:16 +0530

    sdcard-images-utils: rpi: scripts: system_upgrade: copy the adsd3500 overlay file
    Signed-off-by: Sivasubramaniyan-Padmanaban_adi <sivasubramaniyan.padmanaban@analog.com>

--- sdcard-images-utils/rpi/scripts/system_upgrade/apply_patch.sh
+++ sdcard-images-utils/rpi/scripts/system_upgrade/apply_patch.sh
@@ -6,9 +6,10 @@ function update_kernel(){
 	
 	chmod 755 bcm2712-rpi-5-b.dtb	
 	sudo cp bcm2712-rpi-5-b.dtb /boot/firmware/
+	sudo cp adsd3500.dtbo /boot/firmware/overlays/
 	sudo cp Image.gz /boot/firmware/kernel_adi.img
 	mkdir -p test
-	tar -xvf modules.tar.gz -C test
+	tar -xvf modules.tar.gz -C test > /dev/null 2>&1
 	sudo cp -rf test/lib/modules/6.12.47* /lib/modules/
 	sudo rm -rf test
 
