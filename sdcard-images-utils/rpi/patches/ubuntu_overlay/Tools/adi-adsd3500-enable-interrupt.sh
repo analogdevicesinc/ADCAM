97e22e4d67d6b6c699f0ccb0fbd73e27ac651bf8
Sivasubramaniyan-Padmanaban_adi <sivasubramaniyan.padmanaban@analog.com>
2025-12-02 16:53:13 +0530

    sdcard-images-utils: rpi: patches: ubuntu_overlay: Tools: add shell script to reset adsd3500
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

---
+++ sdcard-images-utils/rpi/patches/ubuntu_overlay/Tools/adi-adsd3500-enable-interrupt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NET_HOST_IO_SEL=gpio628
+HOST_IO_DIR=gpio631
+
+# Set 0: EXT_FSYNC / 1: ISP_INT
+echo 1 > /sys/class/gpio/$NET_HOST_IO_SEL/value
+
+# Set 0: EXT_FSYNC / 1: ISP_INT
+echo 1 > /sys/class/gpio/$HOST_IO_DIR/value
+
+# Enable interrupt support
+#v4l2-ctl --set-ctrl=fsync_trigger=1 -d /dev/v4l-subdev1
