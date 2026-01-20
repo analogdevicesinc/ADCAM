a89420e5c2ac25da9efbdc180a0c885645ed9483
Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-01-20 14:21:06 +0530

    sdcard-images-utils: nvidia: patches: ubuntu_overlay: dynamically discover v4l-subdev using media-ctl command
    Signed-off-by: Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adi-adsd3500-enable-interrupt.sh
+++ sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adi-adsd3500-enable-interrupt.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+MEDIA_DEVICE="/dev/media0"
+DOT_OUTPUT=$(media-ctl -d "$MEDIA_DEVICE" --print-dot)
+DOT_OUTPUT=$(echo "$DOT_OUTPUT" | sed 's/\\n/\n/g')
+SUBDEV_PATH=$(echo "$DOT_OUTPUT" | awk '/adsd3500/ {getline; if ($0 ~ /\/dev\/v4l-subdev/) print $1}' | tr -d '",')
+VIDEO_PATH=$(echo "$DOT_OUTPUT" | awk '/vi-output, adsd3500/ {getline; if ($0 ~ /\/dev\/video/) print $1}' | tr -d '",')
+
+if [[ -n "$SUBDEV_PATH" && -n "$VIDEO_PATH" ]]; then
+    echo "adsd3500 subdev: $SUBDEV_PATH" > /dev/null
+    echo "adsd3500 video : $VIDEO_PATH"  > /dev/null
+else
+    echo "adsd3500 device not found in $MEDIA_DEVICE"
+    exit 1
+fi
+
 # export NET HOST_IO_SEL Pin
 if [ ! -d /sys/class/gpio/gpio305 ]
 then
@@ -21,4 +35,4 @@ echo 1 > /sys/class/gpio/gpio305/value
 echo 1 > /sys/class/gpio/gpio308/value
 
 # Enable interrupt support
-v4l2-ctl --set-ctrl=fsync_trigger=1 -d /dev/v4l-subdev1
+v4l2-ctl --set-ctrl=fsync_trigger=1 -d $SUBDEV_PATH
