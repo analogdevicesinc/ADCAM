f3e5b4114bf4f36d717e1a40b32145ff193d096d
Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-03-10 23:19:06 +0530

    sdcard-images-utils: nvidia: patches: ubuntu_overlay: added shell scripts  to capture processed and raw frames
    Signed-off-by: Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

---
+++ sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adsd3500_get_frame/ADTF3175DUAL/raw/get_raw_frame_RAW12_ADTF3175D_mode6.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MEDIA_DEVICE="/dev/media0"
+DOT_OUTPUT=$(media-ctl -d "$MEDIA_DEVICE" --print-dot)
+DOT_OUTPUT=$(echo "$DOT_OUTPUT" | sed 's/\\n/\n/g')
+SUBDEV_PATH=$(echo "$DOT_OUTPUT" | awk '/adsd3500/ {getline; if ($0 ~ /\/dev\/v4l-subdev/) print $1}' | tr -d '",')
+VIDEO_PATH=$(echo "$DOT_OUTPUT" | awk '/vi-output, adsd3500/ {getline; if ($0 ~ /\/dev\/video/) print $1}' | tr -d '",')
+
+if [[ -n "$SUBDEV_PATH" && -n "$VIDEO_PATH" ]]; then
+    echo "adsd3500 subdev: $SUBDEV_PATH" > /dev/null
+    echo "adsd3500 video : $VIDEO_PATH"  > /dev/null
+else
+    echo "adsd3500 device not found in $MEDIA_DEVICE"
+    exit 1
+fi
+
+nr_frames=${1:-1}
+
+v4l2-ctl --set-ctrl=operating_mode=6 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=phase_depth_bits=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=ab_bits=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=confidence_bits=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=ab_averaging=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=depth_enable=0 -d $SUBDEV_PATH
+v4l2-ctl --device /dev/video0 --set-fmt-video=width=1024,height=1536,pixelformat=RG12 --stream-mmap --stream-to=raw_mode6.bin --stream-count=$nr_frames
