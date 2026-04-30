fe940024bf5610eaa2c14f6d665f75a333141874
Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-04-30 19:29:37 +0530

    sdcard-images-utils: rpi: runme.sh: include patch_validation script in release package
    Update the RPi runme.sh packaging flow to bundle patch_validation.sh
    so users can run post-install validation checks on Raspberry Pi.
    
    Signed-off-by: Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- sdcard-images-utils/rpi/runme.sh
+++ sdcard-images-utils/rpi/runme.sh
@@ -466,6 +466,15 @@ create_package() {
         log_warning "apply_patch.sh not found at ${apply_script}"
     fi
 
+    # Copy patch_validation.sh script
+    local validation_script="${ROOTDIR}/scripts/system_upgrade/patch_validation.sh"
+    if [[ -f "${validation_script}" ]]; then
+        cp "${validation_script}" "${PATCH_DIR}/" || log_warning "Failed to copy patch_validation.sh"
+        log_info "Copied: patch_validation.sh"
+    else
+        log_warning "patch_validation.sh not found at ${validation_script}"
+    fi
+
     local archive_name="RPI_ToF_ADSD3500_REL_PATCH_$(date +"%d%b%y").zip"
     local patch_dir_name=$(basename "${PATCH_DIR}")
     local build_dir=$(dirname "${PATCH_DIR}")
