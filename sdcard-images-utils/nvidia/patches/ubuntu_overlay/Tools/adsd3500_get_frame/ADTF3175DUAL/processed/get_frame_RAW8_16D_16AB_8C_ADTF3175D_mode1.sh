2f365983cd027b7509d7f15811509206f7ff7ef8
Nkotihal <nkotihal@example.com>
2026-06-11 17:57:33 +0530

    Modifying the v4l2 scripts as per ADCAM 1.0.0
    Signed-off-by: Nkotihal <nkotihal@example.com>

--- sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adsd3500_get_frame/ADTF3175DUAL/processed/get_frame_RAW8_16D_16AB_8C_ADTF3175D_mode1.sh
+++ sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adsd3500_get_frame/ADTF3175DUAL/processed/get_frame_RAW8_16D_16AB_8C_ADTF3175D_mode1.sh
@@ -19,7 +19,7 @@ nr_frames=${1:-1}
 v4l2-ctl --set-ctrl=operating_mode=1 -d $SUBDEV_PATH
 v4l2-ctl --set-ctrl=phase_depth_bits=6 -d $SUBDEV_PATH
 v4l2-ctl --set-ctrl=ab_bits=6 -d $SUBDEV_PATH
-v4l2-ctl --set-ctrl=confidence_bits=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=confidence_bits=2 -d $SUBDEV_PATH
 v4l2-ctl --set-ctrl=ab_averaging=0 -d $SUBDEV_PATH
 v4l2-ctl --set-ctrl=depth_enable=1 -d $SUBDEV_PATH
-v4l2-ctl --device $VIDEO_PATH --set-fmt-video=width=1024,height=4096,pixelformat=RGGB --stream-mmap --stream-to=mode1.bin --stream-count=$nr_frames
+v4l2-ctl --device $VIDEO_PATH --set-fmt-video=width=3072,height=1707,pixelformat=RGGB --stream-mmap --stream-to=mode1.bin --stream-count=$nr_frames
