aaf39756dbf50abb06d7e4fbf425deaba20a3b18
Nkotihal <nkotihal@example.com>
2026-06-11 17:57:33 +0530

    Modifying the v4l2 scripts as per ADCAM 1.0.0
    Signed-off-by: Nkotihal <nkotihal@example.com>

--- sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adsd3500_get_frame/ADTF3066DUAL/processed/get_frame_RAW8_16D_16AB_8C_ADTF3066_mode0.sh
+++ sdcard-images-utils/nvidia/patches/ubuntu_overlay/Tools/adsd3500_get_frame/ADTF3066DUAL/processed/get_frame_RAW8_16D_16AB_8C_ADTF3066_mode0.sh
@@ -16,10 +16,10 @@ fi
 
 nr_frames=${1:-1}
 
-v4l2-ctl --set-ctrl=operating_mode=0 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=phase_depth_bits=6 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=ab_bits=6 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=confidence_bits=2 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=ab_averaging=1 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=depth_enable=1 -d /dev/v4l-subdev1
-v4l2-ctl --device /dev/video0 --set-fmt-video=width=2560,height=640,pixelformat=RGGB --stream-mmap --stream-to=mode0.bin --stream-count=$nr_frames
+v4l2-ctl --set-ctrl=operating_mode=0 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=phase_depth_bits=6 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=ab_bits=6 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=confidence_bits=2 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=ab_averaging=1 -d $SUBDEV_PATH
+v4l2-ctl --set-ctrl=depth_enable=1 -d $SUBDEV_PATH
+v4l2-ctl --device $VIDEO_PATH --set-fmt-video=width=2560,height=640,pixelformat=RGGB --stream-mmap --stream-to=mode0.bin --stream-count=$nr_frames
